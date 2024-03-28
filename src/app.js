a4b0fc246d4bb93cf02c1b76bc166f395ad1e98a
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-03-28 00:36:27 +0000

    Excuse Generator Without Style

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandomWord(arr) {
+  let randomWord = Math.floor(Math.random() * arr.length);
+  return arr[randomWord] + " ";
+}
+
+const buttonExcuse = document.getElementById("excuseButton");
+
+buttonExcuse.addEventListener("click", function() {
+  location.reload();
+});
+
+window.onload = () => {
+  document.getElementById("excuse1").innerHTML = "Oh my GOD !!!";
+  document.getElementById("excuse").innerHTML =
+    getRandomWord(who) +
+    getRandomWord(action) +
+    getRandomWord(what) +
+    getRandomWord(when);
 };
