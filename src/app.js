4c871edfd5860b3ea96c2e62f7580d044f991098
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-04-01 20:27:45 +0000

    Excuse Generator Done

--- src/app.js
+++ src/app.js
@@ -16,19 +16,19 @@ let when = [
   "while I was praying"
 ];
 
-function getRandomWord(arr) {
-  let randomWord = Math.floor(Math.random() * arr.length);
-  return arr[randomWord] + " ";
-}
-
+//document.getElementById("excuse1").innerHTML = "Oh my GOD !!!";
 const buttonExcuse = document.getElementById("excuseButton");
 
 buttonExcuse.addEventListener("click", function() {
   location.reload();
 });
 
+function getRandomWord(arr) {
+  let randomWord = Math.floor(Math.random() * arr.length);
+  return arr[randomWord] + " ";
+}
+
 window.onload = () => {
-  document.getElementById("excuse1").innerHTML = "Oh my GOD !!!";
   document.getElementById("excuse").innerHTML =
     getRandomWord(who) +
     getRandomWord(action) +
